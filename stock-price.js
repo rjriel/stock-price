8ee2be9b94ca2598b3e396434588ac66ddba803b
Rene-Jacques Riel <r@r87.ca>
2016-04-05 10:26:53 -0400

    added commenting

--- stock-price.js
+++ stock-price.js
@@ -1,8 +1,13 @@
 var getMaxProfit = function(arr) {
   var diff;
+
+  // find the index of the smallest number
   var sort_array = arr.slice();
   sort_array.sort((a,b)=>a-b);
   var min_idx = arr.indexOf(sort_array[0]);
+  
+  // find the largest number after the smallest and
+  // get the difference
   var new_array = arr.slice(min_idx+1);
   if (new_array.length == 0)
     diff = -1;
@@ -10,9 +15,14 @@ var getMaxProfit = function(arr) {
     new_array.sort((a,b)=>a-b);
     diff = new_array[new_array.length-1] - sort_array[0];
   }
-  var prev = arr.splice(0,min_idx);
+
+  // create a new array consisting of the numbers before the smallest
+  var prev = arr.slice(0,min_idx);
   if (prev.length == 0)
     return diff;
+
+  // recursively call this function with the new array
+  // and compare results
   var next_diff = getMaxProfit(prev);
   return diff > next_diff ? diff : next_diff;
 };
